33db43b5f2236825b0d9f9880bfe414ecfa92e78
Yaswant <yaswantsoni2005@gmail.com>
2026-04-03 10:51:48 +0530

    fix: routes fixed

--- src/app.js
+++ src/app.js
@@ -3,8 +3,14 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import morgan from "morgan";
-import {errorMiddleware} from "./middlewares/error.middleware.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
 import { rateLimiter } from "./middlewares/rateLimiter.middleware.js";
+
+// routes
+import authRoutes from "./routes/auth.route.js";
+import recordRoutes from "./routes/record.routes.js";
+import categoryRoutes from "./routes/category.routes.js";
+import dashboardRoutes from "./routes/dashboard.routes.js";
 
 const app = express();
 
@@ -14,14 +20,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("dev"));
 app.use(cors());
 
-// rate limiters 
-app.use(rateLimiter.global);
-app.use("/api/v1/auth", rateLimiter.auth, authRoutes);
-app.use("/api/v1/records", rateLimiter.record, recordRoutes);
-app.use("/api/v1/categories", rateLimiter.category, categoryRoutes);
-app.use("/api/v1/dashboard", rateLimiter.dashboard, dashboardRoutes);
-
-// Body parsers
+// Body parsers 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
@@ -29,20 +28,17 @@ app.use(cookieParser());
 
 // health route
 app.get("/", (req, res) => {
-  res.json({ status: "ok", timestamp: new Date() });
+  res.json({ status: "ok", uptime: process.uptime(), timestamp: new Date().toISOString() });
 });
 
-// Routes
-import authRoutes from "../src/routes/auth.route.js"
-import recordRoutes from "../src/routes/record.routes.js";
-import categoryRoutes from "../src/routes/category.routes.js";
-import dashboardRoutes from "../src/routes/dashboard.routes.js";
+// rate limiters
+app.use(rateLimiter.global);
+app.use("/api/v1/auth", rateLimiter.auth, authRoutes);
+app.use("/api/v1/records", rateLimiter.record, recordRoutes);
+app.use("/api/v1/categories", rateLimiter.category, categoryRoutes);
+app.use("/api/v1/dashboard", rateLimiter.dashboard, dashboardRoutes);
 
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/records", recordRoutes);
-app.use("/api/v1/categories", categoryRoutes)
-app.use("/api/v1/dashboard", dashboardRoutes);
-
+// Error handler
 app.use(errorMiddleware);
 
 export default app;
